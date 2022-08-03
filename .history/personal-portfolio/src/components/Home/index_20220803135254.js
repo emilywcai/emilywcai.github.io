355402801be1ba72e6aafc7352a0d40242760fce
Emily Cai <emilyw.cai@gmail.com>
2022-08-03 15:03:18 -0400

    name animation

---
+++ .history/personal-portfolio/src/components/Home/index_20220803135254.js
@@ -0,0 +1,16 @@
+import LogoName from '../../assets/EmilyCursiveNoBg.png'
+
+const Home = () => {
+    
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoName} alt="name" />
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ .history/personal-portfolio/src/components/Home/index_20220803135309.js
@@ -0,0 +1,17 @@
+import LogoName from '../../assets/EmilyCursiveNoBg.png'
+
+const Home = () => {
+    
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoName} alt="name" />
+                <br />
+                software engineering student
+            </div>
+        </div>
+    )
+}
+
+export default Home
